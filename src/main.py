08d3018b20401d7833a2dc7dc742451d20cee50e
Zhengkang Huang <zkhuang@hust.edu.cn>
2023-07-06 01:13:32 +0800

    Add Fixes Tag check
    Signed-off-by: Zhengkang Huang <zkhuang@hust.edu.cn>

--- src/main.py
+++ src/main.py
@@ -2,8 +2,16 @@
 
 import checker
 import argparse
+import getpass
+import re
+from git import Repo
+
+linux = '/home/' + getpass.getuser() + "/repo/linux"
+pattern = re.compile(r'Fixes:[ ]?(commit )?([0-9a-fA-F]{12,})')
 
 if __name__ == '__main__':
+    repo = Repo(linux)
+    
     arg_parser = argparse.ArgumentParser(description='Output some necessary information after parsing.', add_help=True)
     arg_parser.add_argument("-p", "--path", type=str, help="The absolute path where the patch is located.")
     
@@ -13,8 +21,12 @@ if __name__ == '__main__':
         raise Exception('no input')
 
     c = checker.SimilarSitesChecker(args.path)
-    c.check_rule1()
-    c.check_rule2()
-    c.check_rule3()
-    print(str(c.patch_info.commit_id) + ' is finished.')
+    commit = repo.commit(c.patch_info.commit_id)
+    if pattern.search(commit.message):
+        c.check_rule1()
+        c.check_rule2()
+        c.check_rule3()
+        print(str(c.patch_info.commit_id) + ' is finished.')
+    else:
+        print(str(c.patch_info.commit_id) + ' is not Fixes Tag patch.')
 
